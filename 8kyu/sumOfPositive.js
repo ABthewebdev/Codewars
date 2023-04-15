95a8129a53985ec1ded01c089ddfa0189e7ec620
Alex Brewster <alexgbrewster@gmail.com>
2023-04-15 10:06:18 -0400

    codewars challenges

---
+++ 8kyu/sumOfPositive.js
@@ -0,0 +1,14 @@
+// You get an array of numbers, return the sum of all of the positives ones
+arr = [-1, -4, 2, 3, 8]
+function positiveSum(arr) {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] > 0) {
+      sum += arr[i];
+    }
+  } return sum;
+}
+
+function positiveSum2(arr) {
+   return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+}
