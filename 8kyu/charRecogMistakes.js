87fe2a273efb680ec550c61393523467e70da504
Alex Brewster <alexgbrewster@gmail.com>
2023-04-21 16:45:33 -0400

    Create charRecogMistakes.js

---
+++ 8kyu/charRecogMistakes.js
@@ -0,0 +1,16 @@
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// My solution
+correct = s => s.replace(/0/g,'O').replace(/1/g,'I').replace(/5/g,'S')
+
+const corrections = {
+	'5': 'S',
+	'0': 'O',
+  '1': 'I',
+};
+
+const correct = string => (
+	string.replace(/[501]/g, character => corrections[character])
+);
