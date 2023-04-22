d45c192c590829dd1d2ab3ffd3971e8b8336d7b6
Alex Brewster <alexgbrewster@gmail.com>
2023-04-22 12:16:13 -0400

    Update checkForFactor.js

--- 8kyu/checkForFactor.js
+++ 8kyu/checkForFactor.js
@@ -1,8 +1,11 @@
 // This function should test if the factor is a factor of base.
 // Return true if it is a factor or false if it is not.
 
-function checkForFactor (base, factor) {
-  return base % factor === 0;
+function checkForFactor(base, factor) {
+  if(base % factor === 0) {
+    return true
+  }
+  else {
+    return false
+  }
 }
-
-const checkForFactor2 = (base, factor) => base % factor === 0;
