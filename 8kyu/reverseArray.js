dfff4d2ae6c29a6842b0fb30c33376bf5fc0dc32
Alex Brewster <alexgbrewster@gmail.com>
2023-04-23 00:00:56 -0400

    Update reverseArray.js

--- 8kyu/reverseArray.js
+++ 8kyu/reverseArray.js
@@ -1,5 +1,11 @@
-let arr = [1, 3, 8]
+// Create a function that takes in an array and returns it in reverse order
+// My solution
+let nums = [1, 3, 8]
 function reverseNums(nums) {
+    return nums.reverse()
+}
+
+function reverseNums2(nums) {
     for(let i = 0; i < nums.length / 2; i++) {
         const num = nums[i];
         nums[i] = nums[nums.length - 1 - i];
@@ -7,4 +13,3 @@ function reverseNums(nums) {
     }
     return nums;
 }
-console.log(reverseNums(arr))
