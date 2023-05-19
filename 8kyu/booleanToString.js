a6d55771c6d9f3559dace66507b255e6bd69c0a8
Alex Brewster <alexgbrewster@gmail.com>
2023-05-19 15:17:56 -0400

    Codewars challenges

---
+++ 8kyu/booleanToString.js
@@ -0,0 +1,9 @@
+// create a function that converts a boolean to a string
+// my solution
+function booleanToString(b) {
+    return b.toString()
+}
+
+function booToString() {
+    return b ? 'true' : 'false'
+}
