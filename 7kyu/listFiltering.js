a9e29f57fe37d0acc4ad5591be0f935a632e143d
Alex Brewster <alexgbrewster@gmail.com>
2023-07-25 17:17:13 -0400

    codewars challenge

---
+++ 7kyu/listFiltering.js
@@ -0,0 +1,9 @@
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+// My solution
+function filter_list(l) {
+    return l.filter(l => String(l) !== l)
+}
+
+function filter_list(l) {
+    return l.filter(function(v) {return typeof v == 'number'})
+  }
--- 8kyu/areaOrPerimeter.js
+++ 8kyu/areaOrPerimeter.js
@@ -1,5 +1,5 @@
 // You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
-// If it is a square, return its area. If it is a rectangle, return its perimeter.
+// If it is a square, return its area. If it is a rectangle, return its perimeter. Assume the input will always meet one of the options.
 const areaOrPerimeter = function(l , w) {
     return l == w ? l*w : 2*(l + w)
 };
