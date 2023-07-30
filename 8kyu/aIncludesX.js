6d46ab2cbe4c7d3c8f3aef2e4feaac95e7d6f4eb
Alex Brewster <alexgbrewster@gmail.com>
2023-07-30 00:30:59 -0400

    codewars challenge

--- 8kyu/aIncludesX.js
+++ 8kyu/aIncludesX.js
@@ -1,7 +1,7 @@
 // Create a function that returns true if x is in the array a
 // My solution
 function check(a, x) {
-    return a.includes(x)
+    return a.includes(x);
 }
 
 const check = (a, x) => a.includes(x);
