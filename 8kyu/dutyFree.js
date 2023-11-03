045caa44d16e69289fd555a01c1624002ed599ba
Alex Brewster <alexgbrewster@gmail.com>
2023-11-03 16:10:20 -0400

    codewars challenge

---
+++ 8kyu/dutyFree.js
@@ -0,0 +1,5 @@
+// The purpose of this kata is to work out just how many bottles of duty free whiskey you would have to buy such that the savings over the normal high street price would effectively cover the cost of your holiday.
+// You will be given the high street price (normPrice, in £ (Pounds)), the duty free discount (discount, in percent) and the cost of the holiday (in £).
+function dutyFree(normPrice, discount, hol){
+    return(Math.floor(hol / normPrice / discount * 100))
+}
