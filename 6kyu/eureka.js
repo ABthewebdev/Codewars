de7b9bda4accd47389930d1e94237f759508e7c5
Alex Brewster <alexgbrewster@gmail.com>
2023-11-05 17:44:42 -0500

    codewars challenge

---
+++ 6kyu/eureka.js
@@ -0,0 +1,15 @@
+// The number 
+89
+89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number: 
+89 = (8 * 1) + (9 squared)
+function sumDigPow(a, b) {
+  var arr = [];
+  for (var i = a; i <= b; i++) {
+    var sum = 0;
+    for (var j = 0; j <= String(i).length; j++) {
+      sum += Math.pow(parseInt(String(i)[j]), j+1);  
+      if (sum == i) arr.push(i);
+    }
+  }
+  return arr;
+}
